ea994e9c13f41b878c84b78ea0e325add8c75f00
ddaq <52245497+ddaq@users.noreply.github.com>
2025-10-24 22:33:23 +0800

    Comment out pushbot feed source addition

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git pushbot https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot' >>feeds.conf.default
+#echo 'src-git pushbot https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
